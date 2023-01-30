c2ddb7268c6acb2c7ef4ac60bbdcbab2fed5b0b5
FelixMau <fmau@posteo.de>
2023-01-30 12:48:51 +0100

    streamlit

--- 000_Groupwork/visualisation.py
+++ 000_Groupwork/visualisation.py
@@ -13,6 +13,9 @@ network.add("Generator", "gen2", bus="bus1", p_nom=15)
 network.add("Generator", "gen3", bus="bus2", p_nom=10)
 network.add("Generator", "gen4", bus="bus2", p_nom=15)
 
+network.add("Load", "load_1", bus="bus1", p_set=15)
+network.add("Load", "load_2", bus="bus2", p_set=20)
+
 st.header("PyPSA Energy System Network")
 
 bus_options = ["bus1", "bus2"]
@@ -42,10 +45,21 @@ else:
     network.generators.loc["gen4", "marginal_cost"] = bus2_gen4_cost
 
 bus_load = st.slider("Bus Load", 0, 100, 50)
-network.buses.loc[bus, "p_set"] = bus_load
+@st.cache
+def update_results():
+    if bus == "bus1":
+        network.generators.loc["gen1", "marginal_cost"] = bus1_gen1_cost
+        network.generators.loc["gen2", "marginal_cost"] = bus1_gen2_cost
+    else:
+        network.generators.loc["gen3", "marginal_cost"] = bus2_gen3_cost
+        network.generators.loc["gen4", "marginal_cost"] = bus2_gen4_cost
 
-network.lopf()
 
+    network.lopf(solver_name="cbc")
+
+    return network
+
+network = update_results()
 st.write("Optimal Generation:")
 if bus == "bus1":
     st.write(network.generators_t.p.loc[:, ["gen1", "gen2"]])
@@ -54,3 +68,28 @@ else:
 
 st.write("Total Cost:")
 st.write(network.objective / 1e6, "EUR million")
+import streamlit as st
+import pypsa
+#
+# network = pypsa.Network()
+#
+# # create buses
+# network.add("Bus", "bus1", v_nom=20)
+# network.add("Bus", "bus2", v_nom=30)
+#
+# # create lines
+# network.add("Line", "line1", bus0="bus1", bus1="bus2", s_nom=10, length=100)
+#
+# # create generators
+# network.add("Generator", "gen1", bus="bus1", p_nom=10)
+# network.add("Generator", "gen2", bus="bus2", p_nom=15)
+#
+# network.add("Load", "load1", bus="bus1", p_set=10)
+#
+# network.lopf()
+#
+# st.write("Optimized total costs: ", network.objective)
+# st.write("Generation per bus: ")
+# st.write(network.generators_t.p.sum(axis=0))
+# st.write("Load per bus: ")
+# st.write(network.loads_t.p.sum(axis=0))
